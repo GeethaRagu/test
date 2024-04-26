b8d5e26099c6ebf0bb242d208b6a9c86410c17f8
GeethaRagu <geethumsp@gmail.com>
2024-04-26 11:06:03 +0530

    updated

---
+++ script.js
@@ -0,0 +1,23 @@
+const wordInput = document.getElementById("wordInput");
+const submitWord = document.getElementById("submitWord");
+const resultArea = document.getElementById("resultDisplay");
+const accordion = document.getElementById("accordionFlushExample");
+
+//function to create elements
+function element(tag, classname, id, text) {
+  let tags = document.createElement(tag);
+  tags.classList = classname;
+  tags.id = id;
+  tags.innerHTML = text;
+  return tags;
+}
+
+//Fetch API when user clicks on search button
+
+submitWord.addEventListener("click", () => {
+  accordion.innerHTML = "";
+  resultArea.setAttribute("style","border:2px solid gray");
+  const word = wordInput.value;
+  console.log(word);
+
+})
