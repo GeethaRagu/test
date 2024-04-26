edfc22b81086dda2590a0fb71ed8a3e2ff459395
GeethaRagu <geethumsp@gmail.com>
2024-04-26 11:27:37 +0530

    updated

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-const wordInput = document.getElementById("wordInput");
-const submitWord = document.getElementById("clickforResult");
-const resultArea = document.getElementById("resultDisplay");
-const accordion = document.getElementById("accordionFlushExample");
+const id = document.getElementById("searchWord");
 
-//function to create elements
-function element(tag, classname, id, text) {
-  let tags = document.createElement(tag);
-  tags.classList = classname;
-  tags.id = id;
-  tags.innerHTML = text;
-  return tags;
-}
 
-//Fetch API when user clicks on search button
+id.addEventListener("click",()=>{
 
-submitWord.addEventListener("click", () => {
-  const word = wordInput.value;
-  alert(word);
-
+    const word = document.getElementById("word");
+    const data = word.value;
+    alert(data);
 })
